47a51953936602e58317a22cb54226d0cc279ba2
Morgan Thibert <morgan.thibert@gmail.com>
2023-11-23 02:02:05 +0100

    deploy: change tags of prod images

--- prod/build-prod.sh
+++ prod/build-prod.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 docker build --no-cache -t cheatsheet-app . -f ./prod/Dockerfile
-docker tag cheatsheet-app harbor.morgan-thibert.com/cheatsheet-app/cheatsheet-app:prod
+docker tag cheatsheet-app harbor.morgan-thibert.com/cheatsheet-app/cheatsheet-app:latest
+# create a tag with the date of build for rollback if needed
+DATE_TAG=$(date +%Y-%m-%d--%H-%M-%S)
+docker tag cheatsheet-app harbor.morgan-thibert.com/cheatsheet-app/cheatsheet-app:$DATE_TAG
+# upload on harbor
+docker push harbor.morgan-thibert.com/cheatsheet-app/cheatsheet-app:latest
+docker push harbor.morgan-thibert.com/cheatsheet-app/cheatsheet-app:$DATE_TAG
